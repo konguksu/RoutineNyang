f71b49246cc650bc47fc25434125ce2d89cbaf00
konguksu <rndbsk9@swu.ac.kr>
2025-05-12 19:00:44 +0900

    [chore] 애플리케이션 실행 및 테스트 클래스

---
+++ backend/src/test/java/com/routinenyang/backend/RoutineNyangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.routinenyang.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineNyangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
